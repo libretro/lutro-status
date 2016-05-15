9520e6f66e9ffe86243b3830787d923c17b3a55c
Rob Loach <robloach@gmail.com>
2016-05-15 10:16:10 -0500

    Write to README.md

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # TODO: Find a clean way to discover where the core is.
 
 lutro-status.txt: love-api clean
-	retroarch -L /usr/lib/libretro/lutro_libretro.so . > lutro-status.txt
+	retroarch -L /usr/lib/libretro/lutro_libretro.so . > README.md
 
 love-api:
 	@git submodule update --init
--- main.lua
+++ main.lua
@@ -1,4 +1,31 @@
 -- check if the api definitions are present in the lib
+header = [[
+# Lutro Status
+
+Compares the [Lutro](https://github.com/libretro/libretro-lutro) API to the [LÖVE API](https://github.com/love2d-community/love-api), resulting in a [`lutro-status`](lutro-status.txt) file.
+
+## Requirements
+
+- [`git`](https://git-scm.com/)
+- [`make`](https://www.gnu.org/software/make/)
+- [RetroArch](http://www.libretro.com)
+- [libretro-lutro](https://github.com/libretro/libretro-lutro)
+
+## Usage
+
+```
+make
+```
+
+### Build Manually
+
+```
+git submodule update --init
+retroarch -L /usr/lib/libretro/lutro_libretro.so . > lutro-status.txt
+```
+
+## Status
+]]
 lib = nil
 local libTxt = ""
 if (love ~= nil) then
@@ -76,6 +103,9 @@ printResults = function (results, level)
 end
 
 lib.load = function ()
+  -- Write the header of the README.md file.
+  io.write(header)
+
   -- Add love-api to the module load path.
   package.path = package.path .. ';love-api/?.lua'
   local API = require("love_api")
